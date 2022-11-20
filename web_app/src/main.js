208cd63dee1cc2dcd907250c54af7c5ccec70e1a
이윤건(Macko) <sangok1993@naver.com>
2022-11-20 15:06:19 +0900

    add bootstrapvue3 and sass

--- web_app/src/main.js
+++ web_app/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import BootstrapVue3 from "bootstrap-vue-3";
 
-createApp(App).use(router).mount('#app')
+import "@/styles/index.scss";
+
+const app = createApp(App);
+app.use(router);
+app.use(BootstrapVue3);
+app.mount("#app");
