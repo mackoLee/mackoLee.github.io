a8c2ee0135a97e3f545c6a1498a7b98b8842ee12
이윤건(Macko) <sangok1993@naver.com>
2023-03-25 23:03:07 +0900

    라우팅 테스트

--- web/create_path.py
+++ web/create_path.py
@@ -9,12 +9,13 @@ index_path = "../index.html"
 if __name__ == '__main__':
     print(index_path)
     with open(index_path, 'r') as index_file:
+        index = index_file.read()
         for path in paths:
             if not os.path.exists(path):
                 os.makedirs(path)
             print("Created path: " + path)
             with open(path + '/index.html', 'w') as f:
-                f.write(index_file.read())
+                f.write(index)
                 f.close()
 
 
