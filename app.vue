f711e8167c9c1a62cd5dc8bc49ffa422f594d8fe
이윤건(Macko) <macko@eipgrid.com>
2023-10-06 15:51:22 +0900

    useHead Test

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
-document.querySelector('html').setAttribute('lang', useI18n().locale.value);
+useHead({
+  htmlAttrs: {
+    lang: useI18n().locale.value,
+  },
+})
+// document.querySelector('html').setAttribute('lang', useI18n().locale.value);
 useDayjs().locale(useI18n().locale.value);
 </script>
 <template>
